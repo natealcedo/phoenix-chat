78dd042f210a62088a2d7bf42cb4480e3f620173
Nathaniel D Alcedo Jr <nathaniel.alcedo@axa.com.sg>
2018-07-08 16:25:15 +0800

    Finish basics and setup CI with travis

--- mix.exs
+++ mix.exs
@@ -10,7 +10,14 @@ defmodule Chat.Mixfile do
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -40,7 +47,9 @@ defmodule Chat.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      # tracking test coverage
+      {:excoveralls, "~> 0.7.0", only: [:test, :dev]}
     ]
   end
 
